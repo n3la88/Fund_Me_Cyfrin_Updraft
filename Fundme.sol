ccdefff302c9b2580b2d871fc00e3c03d791d186
n3la88 <mari.montab@gmail.com>
2024-11-11 14:33:57 -0600

    4.Solidity reverts

--- Fundme.sol
+++ Fundme.sol
@@ -3,11 +3,13 @@ pragma solidity ^0.8.18;
 
 contract FundMe {
 
+    uint256 public minUSD = 5;
+
     function fund() public payable{
-        //allow users to send $
+        //allow users to send $5
         //have a minimum $ to send
 
-        require(msg.value > 1e18, "Did not send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 *10 ** 18
+        require(msg.value >= minUSD, "Did not send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 *10 ** 18
 
 
     }
