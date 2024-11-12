8695a24cc63709387651f2cdc901a18cbcf61554
n3la88 <mari.montab@gmail.com>
2024-11-12 16:28:49 -0600

    13. Msg sender explained

--- Fundme.sol
+++ Fundme.sol
@@ -7,13 +7,17 @@ contract FundMe {
 
     uint256 public minUSD = 5 * 1e10; // or  uint256 public minUSD = 5e10
 
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
     function fund() public payable{
         //allow users to send $5
         //have a minimum $ to send
 
         require(getConversionRate(msg.value) >= minUSD, "Did not send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 *10 ** 18
-
-    
+        // 
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
     function getPrice() public view returns(uint256) {
@@ -22,7 +26,7 @@ contract FundMe {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         ( , int256 answer,  ,  ,  ) = priceFeed.latestRoundData();
         // Price of ETH in terms of USD
-        // 2000.000000000
+        // 2000000000000000000
         return uint256(answer * 1e10);
     }
 
@@ -34,6 +38,10 @@ contract FundMe {
         // $2000 = 1 ETH
         uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e10;
         return ethAmountInUSD;
+    }
+
+    function getVersion() public view returns (uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 
 
