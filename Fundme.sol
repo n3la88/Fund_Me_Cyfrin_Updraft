e32deeeaa07f7a6f829f05bbf69d6174b40bc29c
n3la88 <mari.montab@gmail.com>
2024-11-14 21:08:49 -0600

    23. Test the smart contract with a testnet

--- Fundme.sol
+++ Fundme.sol
@@ -12,6 +12,13 @@ contract FundMe {
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
+    address public owner;
+
+    //Function that is called inmediately 
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable{
         //allow users to send $5
         //have a minimum $ to send
@@ -22,15 +29,36 @@ contract FundMe {
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
+   
         // for loop(starting index, ending index, step amount)
         for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
+
+        //reset the array
+        funders = new address[](0);
+
+        //withdraw the funds - three methods
+/*
+        //1. Transfer (2300 gas, if it fails, it errors)
+        payable(msg.sender).transfer(address(this).balance);
+        
+        //2. Send (2300 gas, returns bool)
+        bool sendSuccess= payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send failed");
+*/
+        //3. Call (forward all gas or set gas, returns bool)
+        (bool callSuccess, )= payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
 
-  
+    //Instead of adding to each function this code: require(msg.sender == owner, "Must be the owner!"); we can use a modifier
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender is not the owner");
+        _; //whatever else is in the function
+    }
 
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Fund_Me_Cyfrin_Updraft-
 Fund_Me_Cyfrin_Updraft 
+
+Checkpoint at step 23 
+Test the smart contract with a testnet
+https://sepolia.etherscan.io/address/0xfEa1D33c5A7ffe8390f2A1CD74bFC961FDEC51F9
